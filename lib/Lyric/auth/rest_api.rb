04afc2756e97d7a2ea552ec3ac4c2decf8257207
Colin <colinmmontgomery@gmail.com>
2017-03-20 17:34:12 -0300

    updates

--- lib/Lyric/auth/rest_api.rb
+++ lib/Lyric/auth/rest_api.rb
@@ -5,9 +5,11 @@ module Lyric
 
     api_address = 'https://api.honeywell.com/v2/'
     def locations
-      response = HTTParty.get("#{api_address}locations",
-                              :headers => {"Authorizatiion" => "Bearer #{self.access_token}"})
-      Lyric::Model::LocationList.new(JSON.parse(response.body))
+      #response = HTTParty.get("#{api_address}locations",
+      #                        :headers => {"Authorizatiion" => "Bearer #{self.access_token}"})
+
+      response = File.get('response.json')
+      Lyric::Model::LocationList.new(JSON.parse(response))
     end
   end
 end
--- spec/Lyric_spec.rb
+++ spec/Lyric_spec.rb
@@ -1,32 +1,16 @@
 require 'spec_helper'
+require 'Lyric/auth/rest_api'
 
 describe Lyric do
   it 'has a version number' do
     expect(Lyric::VERSION).not_to be nil
   end
 
-  it 'has a client' do
-    expect(Lyric::Client).not_to be nil
-  end
-
-  it 'has RestAPIMethods' do
+  it 'has a RestAPIMethods module' do
     expect(Lyric::RestAPIMethods).not_to be nil
   end
 
-  it 'has a list of locations class' do
-    expect(Lyric::Model::LocationList).not_to be nil
+  it 'finds a response' do
+    expect(locations).not_to be nil
   end
-
-  it 'has a location class' do
-    expect(Lyric::Model::Location).not_to be nil
-  end
-
-  it 'has a list of devices class' do
-    expect(Lyric::Model::DeviceList).not_to be nil
-  end
-
-  it 'has a device class' do
-    expect(Lyric::Model::Device).not_to be nil
-  end
-
 end
