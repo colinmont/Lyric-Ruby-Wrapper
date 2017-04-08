1a6bc8dd2374d3243b8eab156c6faecd76d6c68f
Colin Montgomery <colinmmontgomery@gmail.com>
2017-04-08 14:07:23 +0000

    remved hard coded token

--- lib/Lyric/auth/rest_api.rb
+++ lib/Lyric/auth/rest_api.rb
@@ -7,7 +7,7 @@ module Lyric
     def user
      # api_address = 'https://api.honeywell.com/v2/'
       response = HTTParty.get("https://api.honeywell.com/v2/locations?apikey=H5lJQeAy7qJeOkhLWovSG1S2uNJIQkzM",
-                              :headers => {"Authorizatiion" => "Bearer wcdr1qNFwat8pgJRCBDSwHE4zrBi"})
+                              :headers => {"Authorizatiion" => "Bearer #{self.token}"})
       
       #response = File.get('response.json')
       user = Lyric::Model::User.new(response)
