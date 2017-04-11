c4458ce42c095bae3b8bf67b83c6667335c13913
Colin Montgomery <colinmmontgomery@gmail.com>
2017-04-11 17:21:27 +0000

    Fix spelling error

--- lib/Lyric/auth/rest_api.rb
+++ lib/Lyric/auth/rest_api.rb
@@ -8,8 +8,8 @@ module Lyric
      # api_address = 'https://api.honeywell.com/v2/'
       response = HTTParty.get("https://api.honeywell.com/v2/locations?apikey=H5lJQeAy7qJeOkhLWovSG1S2uNJIQkzM",
                               :headers => {"Authorization" => "Bearer #{self.token}"})
-      json = JSON.parse(response)
-      Lyric::Model::User.new(json)
+
+      Lyric::Model::User.new(response)
     end
   end
 end
