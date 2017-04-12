949db8617e93323ed5daf669f895d3b07429a63d
Colin Montgomery <colinmmontgomery@gmail.com>
2017-04-12 17:10:11 +0000

    cleaned up api calls, added refresh token function

--- lib/Lyric/auth/rest_api.rb
+++ lib/Lyric/auth/rest_api.rb
@@ -6,8 +6,16 @@ module Lyric
   
     def user
      # api_address = 'https://api.honeywell.com/v2/'
-      response = HTTParty.get("https://api.honeywell.com/v2/locations?apikey=H5lJQeAy7qJeOkhLWovSG1S2uNJIQkzM",
-                              :headers => {"Authorization" => "Bearer #{self.access_token}"})
+     
+        headers = {
+            "Authorization" => "Bearer #{self.access_token}"
+        }
+        query = {
+            "apikey" => H5lJQeAy7qJeOkhLWovSG1S2uNJIQkzM
+        }
+        
+        response = HTTParty.get("https://api.honeywell.com/v2/locations",
+                              :headers => headers, :query => query)
         
         if response.key?("code")
             response['message']
@@ -16,7 +24,20 @@ module Lyric
         end
     end
     
-    def refresh_token(refresh_token)
+    def refresh_token
+        
+        headers = {
+            "Authorization" => "Basic SDVsSlFlQXk3cUplT2toTFdvdlNHMVMydU5KSVFrek06SGlwclp0YnRzYTh2WkJkZA=="
+        }
+        
+        body = {
+            "grant_type" => "refresh_token",
+            "refresh_token" => self.refresh_token
+        }
+
+        
+        HTTParty.get("https://api.honeywell.com/oauth2/token",
+                              :headers => headers, :body => body)
     end
     
   end
--- lib/Lyric/client.rb
+++ lib/Lyric/client.rb
@@ -3,13 +3,12 @@ require_relative 'auth/rest_api'
 module Lyric
   class Client
 
-    def initialize(access_token = nil)
+    def initialize(access_token = nil, refresh_token = nil)
       @access_token = access_token
+      @refresh_token = refresh_token
     end
 
-    attr_reader :access_token
-
+    attr_reader :access_token, :refresh_token
     include RestAPIMethods
-
   end
 end
