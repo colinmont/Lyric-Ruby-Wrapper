0f250766566f509eecc28945caf8412e7e37490a
Colin Montgomery <colinmmontgomery@gmail.com>
2017-04-12 15:51:29 +0000

    adding in fix for expired token

--- lib/Lyric/auth/rest_api.rb
+++ lib/Lyric/auth/rest_api.rb
@@ -8,8 +8,15 @@ module Lyric
      # api_address = 'https://api.honeywell.com/v2/'
       response = HTTParty.get("https://api.honeywell.com/v2/locations?apikey=H5lJQeAy7qJeOkhLWovSG1S2uNJIQkzM",
                               :headers => {"Authorization" => "Bearer #{self.access_token}"})
-
-      Lyric::Model::User.new(response)
+        if response['code'] == 401
+            response['message']
+        else
+            Lyric::Model::User.new(response)
+        end
     end
+    
+    def refresh_token(refresh_token)
+    end
+    
   end
 end
